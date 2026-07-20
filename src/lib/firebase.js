48c1a0b2099f5a4d72b06efb34713735b2878ccb
Trisha <workwithtrisha@users.noreply.github.com>
2026-07-20 18:40:47 +0800

    Add production Firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace this with your actual firebaseConfig from the Firebase Console!
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "943459612657", // The project ID provided
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDNhAGIUWnKCu2D2cyG8LW6g4Ohzk9pEOE",
+  authDomain: "budget-tracker-ee97a.firebaseapp.com",
+  projectId: "budget-tracker-ee97a",
+  storageBucket: "budget-tracker-ee97a.firebasestorage.app",
+  messagingSenderId: "943459612657",
+  appId: "1:943459612657:web:237cff8ff6d42b31c47650",
+  measurementId: "G-MHTYY79VX6"
 };
 
 const app = initializeApp(firebaseConfig);
